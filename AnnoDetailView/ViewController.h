9be65c12e9867e7758c4734efee1d62ee7e378f7
seraphism <yzj9@hotmail.com>
2013-05-28 12:51:10 +0800

    something from scatch
    basic preparations for annotation view

---
+++ AnnoDetailView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AnnoDetailView
+//
+//  Created by Seraphism on 13-5-28.
+//  Copyright (c) 2013年 self.seraphism.inner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AnnoDetailView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AnnoDetailView
+//
+//  Created by Seraphism on 13-5-28.
+//  Copyright (c) 2013年 self.seraphism.inner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
